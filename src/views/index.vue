2c7161ebf2d851378d45805d50d8cea5c3419d90
YSBjwj.123 <jiangweijian09@qq.com>
2023-05-10 16:58:52 +0800

    first commit

---
+++ src/views/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app-container home">
+    Hello World
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data() {
+    return {
+      // 版本号
+      version: "3.8.5"
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style scoped lang="scss">
+.home {
+
+}
+</style>
+
